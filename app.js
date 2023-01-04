0d020fa180cc4847431cbab7daba048056c5c3e4
Lio <gomezlionelsebastian@gmail.com>
2023-01-04 18:44:14 -0300

    Cambio de nombre home.html a index.html

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, '/views/home.html'));
+    res.sendFile(path.join(__dirname, '/views/index.html'));
 })
 
 app.get('/registro', (req, res) =>{
