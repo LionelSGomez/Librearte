cc51e2f37fc96d5954997fd066a474528b4be360
Lio <gomezlionelsebastian@gmail.com>
2023-01-25 23:50:29 -0300

    Ahora los productos se escriben en el json principal, pero sin imagen

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,17 +1,5 @@
-const { json } = require('express');
 const fs = require('fs');
 const path = require('path');
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-    destination: function(req, file, cb){
-        cb(null, './public/images/products');
-    },
-    filename: function(req, file, cb){
-        cb(null, `${newProduct.id}-${Date.now()}-${path.extname(file.originalname)}`)
-    }
-});
-const upload = multer({storage});
 
 const productFile = fs.readFileSync(path.join(__dirname, '../models/products.json'), 'utf-8');
 const listProduct = JSON.parse(productFile);
@@ -28,7 +16,7 @@ const controller = {
     },
     productAdd: (req, res) => {      
         const newProduct = {...req.body}; //obtengo los datos del formulario en la variable newProduct
-        if(newProduct.id){ //condicion de que el formulario no se haya enviado vacio por recargar
+        if(newProduct.id == listProduct.length + 1){ //condicion de que el formulario no se haya enviado vacio por recargar
             newProduct.id = Number(newProduct.id); //cambio el id de string a numero
             newProduct.precio = Number(newProduct.precio); //cambio el precio de string a numero
             for(const property in newProduct){ //itera las propiedades del objeto, si están vacias, serán null
@@ -36,10 +24,10 @@ const controller = {
                     newProduct[property] = null;
                 }
             }
-            listProduct.push(newProduct);
+            listProduct.push(newProduct); //agrego el producto creado al array
             const newProductJson = JSON.stringify(listProduct); //convierto a json
-            fs.writeFileSync('./models/newProducts.json', newProductJson); //creo o sobreescribo newProducts.json, con el producto creado
-            res.redirect('/products')
+            fs.writeFileSync('./models/products.json', newProductJson); //creo o sobreescribo newProducts.json, con el producto creado
+            res.redirect('/products');
         }; 
         res.render('./products/productAdd', {productsList : listProduct, newProduct}); 
         
--- routers/products.js
+++ routers/products.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+// const path = require('path');
+// const multer = require('multer');
+
+// const storage = multer.diskStorage({
+//     destination: function(req, file, cb){
+//         cb(null, './public/images/products');
+//     },
+//     filename: function(req, file, cb){
+//         cb(null, `${Date.now()}-${path.extname(file.originalname)}`)
+//     }
+// });
+// const upload = multer({storage});
+
 const productsController = require('../controllers/productsControllers');
 
 router.get('/', productsController.index);
 router.get('/create/', productsController.productAdd);
-router.post('/create/', upload.array(''), productsController.productAdd,);
+router.post('/create/', productsController.productAdd);
 router.get('/:id', productsController.productDetail);
 router.get('/:id/edit', productsController.productEdit);
 
