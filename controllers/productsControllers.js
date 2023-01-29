1c845b1605e8bbf767757be9dfe1db15ddcd4c94
Lio <gomezlionelsebastian@gmail.com>
2023-01-29 13:58:58 -0300

    guardado de imagen, escritura en json y nuevo boton de creacion de producto

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -14,26 +14,32 @@ const controller = {
         const product = listProduct.find((product) => product.id == id)
         res.render('./products/productDetail', {product} );
     },
-    productAdd: (req, res) => {      
+    productAdd: (req, res) => {
+        res.render('./products/productAdd', {productsList : listProduct});                
+    },
+    store: (req, res) => {
         const newProduct = {...req.body}; //obtengo los datos del formulario en la variable newProduct
-        if(newProduct.id == listProduct.length + 1){ //condicion de que el formulario no se haya enviado vacio por recargar
+        const image = req.file; 
+        if(newProduct.id == listProduct.length + 1){ //condicion de que el formulario no se haya enviado vacio por recargar y que sea el siguiente producto
             newProduct.id = Number(newProduct.id); //cambio el id de string a numero
             newProduct.precio = Number(newProduct.precio); //cambio el precio de string a numero
-            newProduct.img1 = "default.png" //deja como imagen default el logo
+            newProduct.img1 = image.filename;
             for(const property in newProduct){ //itera las propiedades del objeto, si están vacias, serán null
                 if(newProduct[property] == ''){
                     newProduct[property] = null;
                 }
             }
+            if(!newProduct.img1){
+                newProduct.img1 = "default.png" //deja como imagen default el logo
+            }
             listProduct.push(newProduct); //agrego el producto creado al array
             const newProductJson = JSON.stringify(listProduct); //convierto a json
             fs.writeFileSync('./models/products.json', newProductJson); //creo o sobreescribo newProducts.json, con el producto creado
-            res.redirect('/products');
-        }; 
-        res.render('./products/productAdd', {productsList : listProduct, newProduct}); 
+        };
+        res.redirect('/products');
         
-               
     },
+
     productEdit: (req, res) => {
         const {id} = req.params;
         const product = listProduct.find((product) => product.id == id)
--- routers/products.js
+++ routers/products.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const router = express.Router();
-
-// const path = require('path');
-// const multer = require('multer');
-
-// const storage = multer.diskStorage({
-//     destination: function(req, file, cb){
-//         cb(null, './public/images/products');
-//     },
-//     filename: function(req, file, cb){
-//         cb(null, `${Date.now()}-${path.extname(file.originalname)}`)
-//     }
-// });
-// const upload = multer({storage});
+const path = require('path');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './public/images/products');
+    },
+    filename: function(req, file, cb){
+        cb(null, `${Date.now()}${path.extname(file.originalname)}`)
+    }
+});
+const upload = multer({storage});
 
 const productsController = require('../controllers/productsControllers');
 
 router.get('/', productsController.index);
 router.get('/create/', productsController.productAdd);
-router.post('/create/', productsController.productAdd);
+router.post('/create/', upload.single('img1'), productsController.store);
 router.get('/:id', productsController.productDetail);
 router.get('/:id/edit', productsController.productEdit);
 
